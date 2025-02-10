9d58d3dbf2249c8e1bb9cb9f2fe084ca94b40f93
aleksandra.rusjaeva <128512614+aleks540@users.noreply.github.com>
2025-02-10 23:10:38 +0100

    Destination page content and styling

--- src/components/navbar/NavLinks.jsx
+++ src/components/navbar/NavLinks.jsx
@@ -8,20 +8,20 @@ const activeLink = ({ isActive }) => (isActive ? "active" : "");
 
 const NavLinks = ({hideMenu}) => {
     return (
-        <nav className="navLinks" aria-label="Main Navigation">
+        <div className="navLinks" >
             <ul  >
-                <li onClick={hideMenu}>
-                    <NavLink to="/" className="activeLink linkHover">
+                <li onClick={hideMenu}  className={activeLink}>
+                    <NavLink to="/" >
                     <span className="textBold text-preset-8">00</span> <span className="text-preset-8">HOME</span>
  
                     </NavLink>
                 </li>
-                <li onClick={hideMenu} className="activeLink linkHover">
+                <li onClick={hideMenu} className={activeLink}>
                     <NavLink to="/destination" >
                     <span className="textBold text-preset-8">01</span> <span className="text-preset-8">DESTINATION</span>                
                     </NavLink>
                 </li>
-                <li onClick={hideMenu} className="activeLink linkHover">
+                <li onClick={hideMenu} className={activeLink}>
                     <NavLink to="/crew" >
                     <span className="textBold text-preset-8">02</span> <span className="text-preset-8">CREW</span>                         
                     </NavLink>
@@ -33,7 +33,7 @@ const NavLinks = ({hideMenu}) => {
                     </NavLink>
                 </li>
             </ul>
-        </nav>
+        </div>
     )
 }
 
--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -1,18 +1,47 @@
 import React from 'react';
 import "./Destination.scss";
 
+//images
+import Moon from "../../assets/destination/image-moon.png"
+
 const Destination = () => {
     return (
         <main className="destination-page">
-            <section className="destination-container">
-                <div className="left-destination">
+            <div className="paddingTop">
+                
+                <div className="pageTitle text-preset-5"><span className='textBold'>01 </span>PICK YOUR DESTINATION</div>
+                <section className="destination-container">
 
-                </div>
-                <div className="right-destination">
 
-                </div>
+                    <div className="image-destination">
+                        <img className='destination_planetImgPlaceholder' src={Moon} alt="" />
 
-            </section>
+                    </div>
+                    <div className="explanation-destination">
+                        <nav className="planetNavigation">
+
+                        </nav>
+                        <div className='destination_text'>
+                            <div className="planet text-preset-2">MOON
+                            </div>
+                            <p className="description text-preset-9">See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+
+                        </div>
+                        <div className="destination_statistik">
+                            <div className="distance_Cont">
+                                <div className="distanceTitle text-preset-7">AVG. DISTANCE</div>
+                                <div className="distance text-preset-6">384,400 KM</div>
+                            </div>
+                            <div className="travelTime_Cont">
+                                <div className="travelTimeTitle text-preset-7">EST. TRAVEL TIME</div>
+                                <div className="travelTime text-preset-6">3 DAYS</div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </section>
+            </div>
         </main>
     )
 }
